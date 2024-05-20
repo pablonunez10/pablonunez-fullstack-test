4dfad22f70865fd84fd1ca6b85a46f863b4d331e
pablonuñez10 <nunezbenitezpablo@gmail.com>
2024-05-20 15:27:26 -0400

    finish test

--- src/app/(customerFacing)/stripe/purchase-success/page.tsx
+++ src/app/(customerFacing)/stripe/purchase-success/page.tsx
@@ -18,32 +18,34 @@ export default async function SuccessPage({searchParams} : {searchParams : {paym
     if(product == null) return notFound()
     const isSuccess = paymentIntent.status === "succeeded"
     return (
-        <div className="max-w-5xl w-full mx-auto space-y-8">
+        <>
             <h1 className="text-4xl font-bold">{isSuccess ? "Success!" : "Error"}</h1>
-        <div className="flex gap-4 items-center">
-            <div className="aspect-video flex-shrink-0 w-1/3 relative">
-                <Image src={product.imagePath} fill alt={product.name} className="object-cover"/>
-            </div>
-            <div>
-                <div className="text-lg">
-                    {formatCurrency(product.priceInCents/100)}
+            <div className="max-w-full mx-auto space-y-8">
+                <div className="flex gap-4 items-center">
+                    <div className="aspect-video flex-shrink-0 w-1/3 relative">
+                        <Image src={product.imagePath} fill alt={product.name} className="object-cover mt-auto" />
+                    </div>
+                    <div>
+                        <div className="text-lg">
+                            {formatCurrency(product.priceInCents/100)}
+                        </div>
+                        <h1 className="text-xl font-bold">{product.name}</h1>
+                        <div className="line-clamp-3 text-muted-foreground">{product.description}</div>
+                    </div>
+                    <Button className="mt-4" size="lg" asChild>
+                        {isSuccess ? (
+                            <a href={`/products/download/${ await createDownloadVerification(product.id)}`}>
+                                Download
+                            </a>
+                        ) : (
+                            <Link href={`/products/${product.id}/purchase`}>
+                                Try Again
+                            </Link>
+                        )} 
+                    </Button>
                 </div>
-                <h1 className="text-xl font-bold">{product.name}</h1>
-                <div className="line-clamp-3 text-muted-foreground">{product.description}</div>
             </div>
-            <Button className="mt-4" size="lg" asChild>
-                {isSuccess ? (
-                    <a href={`/products/download/${ await createDownloadVerification(product.id)}`}>
-                        Download
-                    </a>
-                ) : (
-                    <Link href={`/products/${product.id}/purchase`}>
-                        Try Again
-                    </Link>
-                )} 
-            </Button>
-        </div>
-    </div>
+        </>
     )
 }
 
